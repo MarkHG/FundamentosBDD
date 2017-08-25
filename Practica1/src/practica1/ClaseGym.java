e16d60ce4bf32d4e4563ce80cb0a9915a865fde7
MarkHG <markhg@ciencias.unam.mx>
2017-08-25 17:50:30 -0500

    Clase gym

--- Practica1/src/practica1/ClaseGym.java
+++ Practica1/src/practica1/ClaseGym.java
@@ -1,12 +1,60 @@
 package practica1;  
 
-
-
 public class ClaseGym {
-   private String nombre;
-   private int id;
-   private String profesor;
-   private 
-           
+    
+    private int id;
+    private String profesor;
+    private String nombre;
+    private static enum dias {LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO; }; 
+    private int horarioInicio;
+    private int horarioFin;
+
+    public ClaseGym(int id, String profesor, String nombre, int horarioInicio, int horarioFin) {
+        this.id = id;
+        this.profesor = profesor;
+        this.nombre = nombre;
+        this.horarioInicio = horarioInicio;
+        this.horarioFin = horarioFin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getHorarioInicio() {
+        return horarioInicio;
+    }
+
+    public void setHorarioInicio(int horarioInicio) {
+        this.horarioInicio = horarioInicio;
+    }
+
+    public int getHorarioFin() {
+        return horarioFin;
+    }
+
+    public void setHorarioFin(int horarioFin) {
+        this.horarioFin = horarioFin;
+    }      
  
 }
